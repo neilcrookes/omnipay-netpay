08266285c78a3bdb0f6acd9993e1ef5299926245
Neil Crookes <neil.crookes@parcelmonkey.com>
2015-04-09 11:30:05 +0100

    Adding getCardReference() method to response

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -25,6 +25,10 @@ class Response extends AbstractResponse
         {
             return $this->data->order->order_id;
         }
+    }
+
+    public function getCardReference()
+    {
         if ( isset( $this->data->token ) )
         {
             return $this->data->token;
